2919f762c7c2df7337f4950cfe82f37ffd8c805a
jir999 <jir.avetisyan@gmail.com>
2020-02-25 21:42:10 +0400

    Change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component{
   render(){
     return (
       <div className="App">
-        <p>asdadadas</p>
+        <p>SaulReznik</p>
       </div>
     );
   }

69935707c67d3f23c68d513941fa16f5b2e3507b
SaulReznik <robhov97@gmail.com>
2020-03-06 22:47:12 +0400

    xara xura baner

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        question: 'Vorna amrigayi mayraqaxaqy',
+        options: [
+            {
+                text: 'Washingtion',
+                isCorrect: true,
+            },
+            {
+                text: 'Los Angelos',
+                isCorrect: false,
+            },
+            {
+                text: 'New York',
+                isCorrect: false,
+            },
+            {
+                text: 'New Orelan',
+                isCorrect: false,
+            }
+        ],
+    },
+    {
+        question: 'Vorna amrigayi mayraqaxaqy',
+        options: [
+            {
+                text: 'Washingtion',
+                isCorrect: true,
+            },
+            {
+                text: 'Los Angelos',
+                isCorrect: false,
+            },
+            {
+                text: 'New York',
+                isCorrect: false,
+            },
+            {
+                text: 'New Orelan',
+                isCorrect: false,
+            }
+        ],
+    },
+]

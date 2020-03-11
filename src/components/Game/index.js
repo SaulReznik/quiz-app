bf867133fc2759cbf10654df8b18c837627e443a
SaulReznik <robhov97@gmail.com>
2020-03-11 23:01:29 +0400

    added randomising

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 import Screen from '../Screen';
 
+function shuffle(array) {
+    var currentIndex = array.length;
+    let temporaryValue; 
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
 const Game = props => {
     const {question, optionClick } = props;
+    let shuffled = shuffle(question.options);
     console.log(question);
 
     return (
@@ -13,9 +35,9 @@ const Game = props => {
 
                 <div id='options-container'>
                     {
-                        question.options.map((item, index) => {
+                        shuffled.map((item, index) => {
                             const { text, isCorrect } = item;
-                            
+
                             return <button
                                 key={index}
                                 onClick={() => optionClick(isCorrect)} 
